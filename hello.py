db2c2a86a527f6a0c0f31762b93b4ddc94659735
Joseph <joseph.carnide@mail.utoronto.ca>
2024-09-26 10:45:40 -0400

    [1.3] - added base.html and custom error pages

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/index')
 def index():
@@ -9,6 +12,14 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
